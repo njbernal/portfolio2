74bd54396d14599198cb5327c654cd68d622deaa
Nick Bernal <nickjbernal@gmail.com>
2022-08-24 08:36:52 -0700

    Added scroll css

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 import { Subscription } from 'rxjs';
-import { APIResponse, Project, CleanProject } from 'src/app/services/models';
-import { environment as env } from 'src/environments/environment';
+import { Project, CleanProject } from 'src/app/services/models';
 
 import { data } from '../services/data'
 
@@ -17,105 +16,6 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   public projects: Array<CleanProject>;
   private projectSub: Subscription;
 
-  // public data = [
-  //   {
-  //     title: "Royalty",
-  //     status: "featured",
-  //     description: "web app",
-  //     links: {
-  //       "links": [
-  //         {
-  //           "name": "Public",
-  //           "url": "http://njb-royalty.s3-website-us-west-1.amazonaws.com/"
-  //         },
-  //         {
-  //           "name": "Github",
-  //           "url": ""
-  //         },
-  //         {
-  //           "name": "More",
-  //           "url": ""
-  //         }
-  //       ]
-  //     },
-  //     tech: {
-  //       "tech": [
-  //         "React",
-  //         "Express",
-  //         "Node",
-  //         "MongoDB",
-  //         "MERN",
-  //         "Python (Automation)",
-  //         "AWS",
-  //         "CI/CD"
-  //       ]
-  //     },
-  //     image: "royalty.png"
-  //   },
-  //   {
-  //     title: "Ludo",
-  //     status: "featured",
-  //     description: "board game",
-  //     links: {
-  //       "links": [
-  //         {
-  //           "name": "Public",
-  //           "url": ""
-  //         },
-  //         {
-  //           "name": "Github",
-  //           "url": ""
-  //         },
-  //         {
-  //           "name": "More",
-  //           "url": ""
-  //         }
-  //       ]
-  //     },
-  //     tech: {
-  //       "tech": [
-  //         "React",
-  //         "Python",
-  //         "Flask",
-  //         "RESTful API",
-  //         "Netlify",
-  //         "Heroku"
-  //       ]
-  //     },
-  //     image: "ludo.png"
-  //   },
-  //   {
-  //     title: "Angular Portfolio",
-  //     status: "featured",
-  //     description: "This portfolio website!",
-  //     links: {
-  //       "links": [
-  //         {
-  //           "name": "Github",
-  //           "url": ""
-  //         },
-  //         {
-  //           "name": "Public",
-  //           "url": ""
-  //         },
-  //         {
-  //           "name": "More",
-  //           "url": ""
-  //         }
-  //       ]
-  //     },
-  //     tech: {
-  //       "tech": [
-  //         "Angular",
-  //         "TypeScript",
-  //         "MaterialUI",
-  //         "Google Cloud",
-  //         "Strapi"
-  //       ]
-  //     },
-  //     image: "website.png"
-  //   }
-  // ]
   constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
